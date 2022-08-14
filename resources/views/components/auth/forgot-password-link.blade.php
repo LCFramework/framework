8c1ce5b308a0389fc53b60625d109dd9c4760dd8
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 11:49:25 +0100

    - Fix link url

--- resources/views/components/auth/forgot-password-link.blade.php
+++ resources/views/components/auth/forgot-password-link.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full flex items-center justify-end">
     <a
-        href="https://beta.surviveourark.com/forgot-password"
+        href="{{route('password.request')}}"
         class="text-sm font-medium text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
     >
         Forgot password
