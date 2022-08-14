73f364787ad6005fac918d80f60b67be7472aea5
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 11:43:24 +0100

    - Styling

--- resources/views/components/auth/forgot-password-link.blade.php
+++ resources/views/components/auth/forgot-password-link.blade.php
@@ -1,9 +1,7 @@
-<div
-    class="w-full flex items-center justify-end"
->
+<div class="w-full flex items-center justify-end">
     <a
         href="https://beta.surviveourark.com/forgot-password"
-        class="text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
+        class="text-sm font-medium text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
     >
         Forgot password
     </a>
