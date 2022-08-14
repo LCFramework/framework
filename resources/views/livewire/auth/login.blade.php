0fb47899d944087ee4807629ea3f98a4be388b21
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 10:25:35 +0100

    - Styling

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit.prevent="authenticate">
     {{$this->form}}
 
-    <x-filament-support::button>
+    <x-filament-support::button class="w-full mt-6">
         Sign in
     </x-filament-support::button>
 </form>
