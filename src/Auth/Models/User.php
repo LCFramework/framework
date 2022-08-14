7f02be16acd82bcf79e3f041089547df65b52bae
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 11:27:24 +0100

    - Change button type to submit

--- src/Auth/Models/User.php
+++ src/Auth/Models/User.php
@@ -7,6 +7,21 @@ use LCFramework\Framework\Auth\Contracts\OptionallyVerifyEmail;
 
 class User extends Authenticatable implements OptionallyVerifyEmail
 {
+    protected $fillable = [
+        'username',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
     public function shouldVerifyEmail(): bool
     {
         return config('lcframework.auth.require_email_verification');
