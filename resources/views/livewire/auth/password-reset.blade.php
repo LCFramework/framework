427f93fc554a101d59ed51e9bcd29e92a5e07020
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 11:24:54 +0100

    - Change button type to submit

--- resources/views/livewire/auth/password-reset.blade.php
+++ resources/views/livewire/auth/password-reset.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit.prevent="submit">
     {{$this->form}}
 
-    <x-filament-support::button class="w-full mt-6">
+    <x-filament-support::button type="submit" class="w-full mt-6">
         Reset password
     </x-filament-support::button>
 </form>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,7 +1,7 @@
 <form wire:submit.prevent="submit">
     {{$this->form}}
 
-    <x-filament-support::button class="w-full mt-6">
+    <x-filament-support::button type="submit" class="w-full mt-6">
         Sign up
     </x-filament-support::button>
 </form>
