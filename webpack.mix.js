d0158b3d5d823f7d757480645c278608669434c0
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-14 09:58:53 +0100

    - Added forms

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,8 @@ mix.setPublicPath('dist');
 mix.sourceMaps();
 mix.version();
 
+mix.js('resources/js/lcframework.js', 'dist/js');
+
 mix.postCss('resources/css/lcframework.css', 'dist/css', [
     require('tailwindcss')
 ]);
