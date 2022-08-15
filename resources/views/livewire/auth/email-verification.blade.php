28362d38261db1a9a7595ee4683babc25fab8469
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 16:58:25 +0100

    - Disable button whilst loading

--- resources/views/livewire/auth/email-verification.blade.php
+++ resources/views/livewire/auth/email-verification.blade.php
@@ -1,7 +1,12 @@
 <form wire:submit.prevent="submit">
     {{$this->form}}
 
-    <x-filament-support::button type="submit" class="w-full mt-6">
+    <x-filament-support::button
+        type="submit"
+        class="w-full mt-6"
+        wire:loading.attr="disabled"
+        wire:target="submit"
+    >
         Resend email
     </x-filament-support::button>
 </form>
