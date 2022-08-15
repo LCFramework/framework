ad192a2e2ce866a624685cc51f0bcce94b01277e
realslimsutton <realslimsutton@users.noreply.github.com>
2022-08-15 13:46:35 +0000

    Fix styling

--- src/Form/Builder/FormBuilder.php
+++ src/Form/Builder/FormBuilder.php
@@ -63,8 +63,7 @@ class FormBuilder
         $name,
         Component|array|Closure $components,
         string $where
-    ): static
-    {
+    ): static {
         $parent = &$this->findParent($this->schema, $name);
         if ($parent === null) {
             return $this;
