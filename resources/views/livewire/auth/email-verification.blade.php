157d87e84b8445db8ceae22aaac7feeac4fa46fb
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 17:15:29 +0100

    - Add logout button

--- resources/views/livewire/auth/email-verification.blade.php
+++ resources/views/livewire/auth/email-verification.blade.php
@@ -1,12 +1,35 @@
-<form wire:submit.prevent="submit">
-    {{$this->form}}
+<div class="space-y-6">
+    <form wire:submit.prevent="submit">
+        {{$this->form}}
+    </form>
 
-    <x-filament-support::button
-        type="submit"
-        class="w-full mt-6"
-        wire:loading.attr="disabled"
-        wire:target="submit"
-    >
-        Resend email
-    </x-filament-support::button>
-</form>
+    <div class="grid md:grid-cols-2 gap-6">
+        <div>
+            <x-filament-support::button
+                class="w-full"
+                wire:click.prevent="submit"
+                wire:loading.attr="disabled"
+                wire:target="submit"
+            >
+                Resend
+            </x-filament-support::button>
+        </div>
+
+        <div>
+            <form action="{{route('logout')}}" method="post">
+                @csrf
+
+                <x-filament-support::button
+                    type="submit"
+                    color="danger"
+                    class="w-full"
+                    wire:loading.attr="disabled"
+                    wire:target="submit"
+                >
+                    Logout
+                </x-filament-support::button>
+            </form>
+        </div>
+    </div>
+
+</div>
