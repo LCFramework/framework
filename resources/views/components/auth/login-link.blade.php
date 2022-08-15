265045d173a0d7c37d528ac45d5b4ee408d5193e
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 15:58:08 +0100

    - Fix nav links

--- resources/views/components/auth/login-link.blade.php
+++ resources/views/components/auth/login-link.blade.php
@@ -1,11 +1,10 @@
-<div class="w-full flex items-center justify-end text-sm font-medium">
+<div class="w-full flex items-center justify-center text-sm font-medium">
     <span>
-        Already have an account?
+        Already have an account? <a
+            href="{{route('login')}}"
+            class="text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
+        >
+            Login here
+        </a>
     </span>
-    <a
-        href="{{route('login')}}"
-        class="text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
-    >
-        Login here
-    </a>
 </div>
