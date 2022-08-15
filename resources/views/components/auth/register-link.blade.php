95d5be3ca7030d442de81484587eb5e9bdd75495
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 17:50:17 +0100

    - Fixed wording

--- resources/views/components/auth/register-link.blade.php
+++ resources/views/components/auth/register-link.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full flex items-center justify-center text-sm font-medium">
     <span>
-        Already have an account? <a
+        Don't have an account? <a
             href="{{route('register')}}"
             class="text-primary-600 transition-colors hover:text-primary-500 focus:text-primary-700"
         >
