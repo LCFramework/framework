9a64be2d2ffb6dfc0fc0e38063204f2372797ffc
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 14:17:31 +0100

    - Fixed FormBuilder

--- src/Form/Builder/FormBuilder.php
+++ src/Form/Builder/FormBuilder.php
@@ -20,25 +20,125 @@ class FormBuilder
         return new static($schema);
     }
 
-    public function get(string|Closure $callback): ?Component
+    public function before($name, Component|array|Closure $components): static
     {
-        $callback = $callback instanceof Closure
-            ? $callback
-            : fn(Component $component): bool => $component instanceof Field && $component->getName() === $callback;
-
-        return collect($this->getFlatSchema($this->schema))
-            ->first($callback);
+        return $this->insert($name, $components, 'before');
     }
 
-    protected function getFlatSchema($schema): array
+    public function after($name, Component|array|Closure $components): static
     {
-        $components = [];
+        return $this->insert($name, $components, 'after');
+    }
 
-        foreach ($schema as $component) {
-            $components[] = $component;
-            $components[] = $this->getFlatSchema($component->getChildComponents());
+    public function prepend(Component|array|Closure $components): static
+    {
+        array_unshift($this->schema, value($components));
+
+        return $this;
+    }
+
+    public function append(Component|array|Closure $components): static
+    {
+        array_push($this->schema, value($components));
+
+        return $this;
+    }
+
+    protected function insert(
+        $name,
+        Component|array|Closure $components,
+        string $where
+    ): static
+    {
+        $parent = &$this->findParent($this->schema, $name);
+        if ($parent === null) {
+            return $this;
         }
 
-        return collect($components)->flatten()->all();
+        $components = value($components);
+
+        $index = $this->findComponentIndex($parent, $name);
+
+        array_splice(
+            $parent,
+            $where === 'before' ? $index : ++$index,
+            0,
+            $components
+        );
+
+        return $this;
+    }
+
+    public function &get(string $name): ?Component
+    {
+        return $this->find($this->schema, $name);
+    }
+
+    public function forget(string $name, Closure $condition = null): static
+    {
+        if ($component = $this->get($name)) {
+            $component
+                ->hidden($condition ?? true)
+                ->disabled($condition ?? true);
+        }
+
+        return $this;
+    }
+
+    public function build(): array
+    {
+        return $this->schema;
+    }
+
+    protected function &find(array &$schema, string $name): ?Component
+    {
+        foreach ($schema as $component) {
+            if ($component instanceof Field && $component->getName() === $name) {
+                return $component;
+            }
+
+            $childComponents = $component->getChildComponents();
+
+            if ($child = $this->find($childComponents, $name)) {
+                return $child;
+            }
+        }
+
+        $default = null;
+
+        return $default;
+    }
+
+    protected function &findParent(array &$schema, string $name): ?array
+    {
+        foreach ($schema as $component) {
+            if ($component instanceof Field && $component->getName() === $name) {
+                return $schema;
+            }
+
+            $childComponents = $component->getChildComponents();
+
+            if ($child = $this->findParent($childComponents, $name)) {
+                return $child;
+            }
+        }
+
+        $default = [];
+
+        return $default;
+    }
+
+    protected function findComponentIndex(array $parent, string $name): int
+    {
+        $index = 0;
+        foreach ($parent as $component) {
+            if ($component instanceof Field && $component->getName() === $name) {
+                return $index;
+            }
+
+            $index++;
+        }
+
+        return $index;
     }
 }
