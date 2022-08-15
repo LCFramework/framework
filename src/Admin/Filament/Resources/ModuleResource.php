422984349704f0fac82733d0ba017c2c2c8822bd
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-15 19:48:46 +0100

    - WIP

--- src/Admin/Filament/Resources/ModuleResource.php
+++ src/Admin/Filament/Resources/ModuleResource.php
@@ -5,6 +5,8 @@ namespace LCFramework\Framework\Admin\Filament\Resources;
 use Filament\Resources\Form;
 use Filament\Resources\Resource;
 use Filament\Resources\Table;
+use Filament\Tables\Columns\BadgeColumn;
+use Filament\Tables\Columns\TextColumn;
 use LCFramework\Framework\Admin\Filament\Resources\ModuleResource\Pages\ListModules;
 use LCFramework\Framework\Module\Models\Module;
 
@@ -20,14 +22,33 @@ class ModuleResource extends Resource
 
     protected static ?int $navigationSort = 0;
 
-    public static function form(Form $form): Form
-    {
-        return $form;
-    }
-
     public static function table(Table $table): Table
     {
-        return $table;
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Name')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('version')
+                    ->label('Version')
+                    ->sortable()
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->label('Description')
+                    ->sortable()
+                    ->searchable()
+                    ->limit(),
+                BadgeColumn::make('status')
+                    ->label('Status')
+                    ->sortable()
+                    ->searchable()
+                    ->colors([
+                        'warning',
+                        'danger' => 'disabled',
+                        'success' => 'enabled'
+                    ])
+            ]);
     }
 
     public static function getPages(): array
