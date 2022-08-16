eac58da08d5663a151ae9962be5e694e818eebec
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-16 15:17:18 +0100

    - WIP

--- src/Installer/Http/Controllers/InstallerController.php
+++ src/Installer/Http/Controllers/InstallerController.php
@@ -3,11 +3,16 @@
 namespace LCFramework\Framework\Installer\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use LCFramework\Framework\LCFramework;
 
 class InstallerController extends Controller
 {
     public function show()
     {
+        if (LCFramework::installed()) {
+            return redirect()->intended();
+        }
+
         return view('lcframework::installer.index');
     }
 }
