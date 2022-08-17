3c87bcd0be578ec4154e863ef5434aad591bcc26
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-17 14:11:19 +0100

    - WIP

--- src/Admin/Filament/Resources/UserResource/Pages/EditUser.php
+++ src/Admin/Filament/Resources/UserResource/Pages/EditUser.php
@@ -2,6 +2,7 @@
 
 namespace LCFramework\Framework\Admin\Filament\Resources\UserResource\Pages;
 
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 use LCFramework\Framework\Admin\Filament\Resources\UserResource;
@@ -17,13 +18,27 @@ class EditUser extends EditRecord
             Action::make('ban')
                 ->label('Ban')
                 ->requiresConfirmation()
-                ->hidden(fn () => $this->record->is_banned)
-                ->action(fn () => $this->record->ban()),
+                ->hidden(fn () => $this->record->is_banned || $this->record->user_code === auth()->id())
+                ->action(function () {
+                    $this->record->ban();
+
+                    Notification::make()
+                        ->success()
+                        ->title('User has been successfully banned')
+                        ->send();
+                }),
             Action::make('unban')
                 ->label('Unban')
                 ->requiresConfirmation()
-                ->hidden(fn () => ! $this->record->is_banned)
-                ->action(fn () => $this->record->unban()),
+                ->hidden(fn () => ! $this->record->is_banned || $this->record->user_code === auth()->id())
+                ->action(function () {
+                    $this->record->unban();
+
+                    Notification::make()
+                        ->success()
+                        ->title('User has been successfully unbanned')
+                        ->send();
+                }),
         ];
     }
 }
