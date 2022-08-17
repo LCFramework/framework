abf1ee7872b6e0a4aee088e3bd5277caf80a9232
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-17 14:06:42 +0100

    - WIP

--- src/Admin/Filament/Resources/UserResource/Pages/EditUser.php
+++ src/Admin/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,7 +5,6 @@ namespace LCFramework\Framework\Admin\Filament\Resources\UserResource\Pages;
 use Filament\Pages\Actions\Action;
 use Filament\Resources\Pages\EditRecord;
 use LCFramework\Framework\Admin\Filament\Resources\UserResource;
-use LCFramework\Framework\Auth\Models\User;
 
 class EditUser extends EditRecord
 {
@@ -18,8 +17,13 @@ class EditUser extends EditRecord
             Action::make('ban')
                 ->label('Ban')
                 ->requiresConfirmation()
-                ->hidden(fn(User $record) => $record->is_banned)
-                ->action(fn(User $record) => $record->ban())
+                ->hidden(fn() => $this->record->is_banned)
+                ->action(fn() => $this->record->ban()),
+            Action::make('unban')
+                ->label('Unban')
+                ->requiresConfirmation()
+                ->hidden(fn() => !$this->record->is_banned)
+                ->action(fn() => $this->record->unban()),
         ];
     }
 }
