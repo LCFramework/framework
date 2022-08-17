bc2b42e8a175c4ec7df1c38d0245d754328cdf25
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-17 05:38:27 +0100

    - WIP

--- database/migrations/2022_08_17_000000_change_bg_users_table.php
+++ database/migrations/2022_08_17_000000_change_bg_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         $tableName = $this->getTableName();
 
-        Schema::create($tableName, function (Blueprint $table) use ($tableName) {
+        Schema::table($tableName, function (Blueprint $table) use ($tableName) {
             $table->unsignedInteger('user_code')->autoIncrement()->change();
             $table->string('passwd')->change();
 
