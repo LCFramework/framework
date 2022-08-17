5822437326a2900b73d802c4dcc9b108435b59b0
realslimsutton <realslimsutton@users.noreply.github.com>
2022-08-17 12:55:00 +0000

    Fix styling

--- src/Admin/Filament/Resources/UserResource/Pages/ListUsers.php
+++ src/Admin/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -3,7 +3,6 @@
 namespace LCFramework\Framework\Admin\Filament\Resources\UserResource\Pages;
 
 use Filament\Resources\Pages\ListRecords;
-use Illuminate\Database\Eloquent\Builder;
 use LCFramework\Framework\Admin\Filament\Resources\UserResource;
 
 class ListUsers extends ListRecords
