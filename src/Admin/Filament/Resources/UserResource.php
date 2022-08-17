6d092158b51579ccfec5aa8f4cd1ada372cadca5
realslimsutton <realslimsutton@users.noreply.github.com>
2022-08-17 06:44:49 +0000

    Fix styling

--- src/Admin/Filament/Resources/UserResource.php
+++ src/Admin/Filament/Resources/UserResource.php
@@ -110,7 +110,7 @@ class UserResource extends Resource
                 BooleanColumn::make('email_verified_at')
                     ->label('Verified')
                     ->sortable()
-                    ->getStateUsing(fn(User $record): bool => $record->hasVerifiedEmail()),
+                    ->getStateUsing(fn (User $record): bool => $record->hasVerifiedEmail()),
                 TextColumn::make('create_date')
                     ->label('Created at')
                     ->date()
