d1102917296c0c466e59cad5342f947651648f7f
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-17 04:13:44 +0100

    - WIP

--- resources/views/livewire/installer/index.blade.php
+++ resources/views/livewire/installer/index.blade.php
@@ -1,13 +1,11 @@
 <form wire:submit.prevent="submit">
     {{$this->form}}
 
-    <div id="exception-modal">
-        <x-filament::modal>
-            <x-slot name="header">
-                Exception
-            </x-slot>
+    <x-filament::modal id="exception-modal">
+        <x-slot name="header">
+            Exception
+        </x-slot>
 
-            {{$this->exceptionMessage}}
-        </x-filament::modal>
-    </div>
+        {{$this->exceptionMessage}}
+    </x-filament::modal>
 </form>
