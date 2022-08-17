80717f2c470ac7becde0262aa7919f25eacc9459
Kieron Sutton <sutton.kieron99@gmail.com>
2022-08-17 02:28:11 +0100

    - WIP

--- src/Installer/InstallerServiceProvider.php
+++ src/Installer/InstallerServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace LCFramework\Framework\Installer;
 
+use Illuminate\Contracts\Encryption\Encrypter as EncrypterInterface;
+use Illuminate\Encryption\Encrypter;
 use Illuminate\Support\ServiceProvider;
 use LCFramework\Framework\Installer\Http\Livewire\Installer;
 use LCFramework\Framework\LCFramework;
@@ -9,6 +11,15 @@ use Livewire\Livewire;
 
 class InstallerServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->app->beforeResolving(EncrypterInterface::class, function ($app) {
+            if ($app['config']->get('app.key') === null) {
+                $app['config']->set('app.key', $this->generateRandomKey());
+            }
+        });
+    }
+
     public function boot(): void
     {
         Livewire::component(
@@ -16,10 +27,17 @@ class InstallerServiceProvider extends ServiceProvider
             Installer::class
         );
 
-        if (! LCFramework::installed()) {
+        if (!LCFramework::installed()) {
             $this->loadRoutesFrom(
-                __DIR__.'/../../routes/installer.php'
+                __DIR__ . '/../../routes/installer.php'
             );
         }
+    }
+
+    protected function generateRandomKey()
+    {
+        return 'base64:' . base64_encode(
+                Encrypter::generateKey(config('app.cipher'))
+            );
     }
 }
